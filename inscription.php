ce8b7d84b1550774188199123b78fdca39bbca52
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-16 10:33:47 +0100

    dossiers images et assets créés, html squelette du formulaire d'inscription créé également

--- inscription.php
+++ inscription.php
@@ -2,3 +2,46 @@ LE FORMULAIRE D'INSCRIPTION POUR MON SITE:
 Le formulaire doit contenir l’ensemble des champs présents dans la table
 “utilisateurs” (sauf “id”) + une confirmation de mot de passe. Dès qu’un utilisateur remplit ce formulaire, les données sont insérées dans la base de
 données et l’utilisateur est redirigé vers la page de connexion.
+
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inscription || TITRE DE MON SITE</title>
+  <link rel="stylesheet" href="UC.css">
+</head>
+<body>
+<form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="name">Login :</label>
+        <input type="text" id="login" name="user_login">
+    </div>
+    <div>
+        <label for="name">Prénom :</label>
+        <input type="text" id="firstname" name="user_firstname">
+    </div>
+    <div>
+        <label for="name">Nom :</label>
+        <input type="text" id="lastname" name="user_lastname">
+    </div>
+    <div>
+        <label for="mail">e-mail :</label>
+        <input type="email" id="mail" name="user_mail">
+    </div>
+    <div>
+        <label for="msg">Mot de passe :</label>
+        <div>Le mdp doit comprendre au moins XXXX,XXXX et XXXXX (quand on clique sur le champs ceci apparait)</div>
+        <input type="password" id="pass" name="password"
+           minlength="8" required>
+
+        <label for="msg">Confirmation du mot de passe :</label>
+        <input type="password" id="pass2" name="password2"
+           minlength="8" required>
+    </div>
+</form>
+<input type="submit" value="Sign in">
+</body>
+</html>
