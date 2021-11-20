3196df29a2daeabd5f4588b4c09e99fbb64baa36
Laura Savickaite <elle.@MacBook-Pro-de-Laura.local>
2021-11-20 15:36:49 +0100

    css inscription en cours

--- inscription.php
+++ inscription.php
@@ -37,7 +37,7 @@ $connect = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
     }
     else {
         //avec le WHERE c'est une comparaison : where la colonne login sql reconnait mon POST login donc ma variable $login ici :: si tu reconnais le même login, alors tu me donneras une erreur
-      $repLogin = mysqli_query($connect, "SELECT `login` FROM `utilisateurs` WHERE `login`= '".$login."'");
+      $repLogin = mysqli_query($connect, "SELECT `login` FROM `utilisateurs` WHERE `login`= '".$login. "'");
       // $repLogin = mysqli_fetch_all($queryLogin, MYSQLI_ASSOC);  
         if(mysqli_num_rows($repLogin)){
           $validation=false;
@@ -45,8 +45,12 @@ $connect = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
       }
     }
 
+    
+    if(empty($password)){
+      $validation=false;
+      $passwordErr .= "Le mot de passe doit être rempli.";
     }
-    if ($confpassword !== $password) {
+    elseif ($confpassword !== $password) {
       $validation=false;
       $confpasswordErr .= "Le mot de passe et sa confirmation ne sont pas les mêmes.";
     } 
@@ -59,6 +63,7 @@ $connect = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
     header('Location:connexion.php');
 
   }
+  }
   
 
 ?>
@@ -70,7 +75,10 @@ $connect = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Inscription || TITRE DE MON SITE</title>
-  <link rel="stylesheet" href="css/inscription.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@200&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="inscription.css">
 </head>
 <body>
     <header>
@@ -79,41 +87,49 @@ $connect = mysqli_connect('localhost', 'root', '', 'moduleconnexion');
 
     <main>
     <article id="form_inscription">
-      <p><span class="error">* ces champs sont obligatoires</span></p>
-      <form action="inscription.php" method="post">
-          <div>
+
+      <section id="champs_inscription"> 
+      <div id="top_part">
+        <h1>BOARDING PASS</h1>
+      </div>
+      <!-- <p><span class="error">* ces champs sont obligatoires</span></p> -->
+      <div id="column">
+      <div id="billet_part1">
+        <form action="inscription.php" method="post">
               <label for="name">Login :</label>
               <input type="text" id="login" name="user_login"> <p><span class="error">*<?php echo $loginErr;?></span></p>
-          </div>
-          <div>
+
             <label for="name">Prénom :</label>
             <input type="text" id="firstname" name="user_firstname">
-          </div>
-          <div>
+
             <label for="name">Nom :</label>
             <input type="text" id="lastname" name="user_lastname">
-          </div>
-          <div>
+
+            <img id="barcode" src="Images/barcode.png">
+        </div>
+        <div id="billet_part2">
+            <hr>
             <label for="msg">Mot de passe :</label>
-          <div>Le mdp doit comprendre au moins XXXX,XXXX et XXXXX (quand on clique sur le champs ceci apparait)</div>
-            <input type="password" id="pass" name="password"minlength="8" required><p><span class="error"> *<?php echo $passwordErr;?></span></p>
+          <!-- <span><p></p>Le mdp doit comprendre au moins XXXX,XXXX et XXXXX (quand on clique sur le champs ceci apparait)</p></span> -->
+            <input type="password" id="pass" name="password" required><p><span class="error"> *<?php echo $passwordErr;?></span></p>
             <label for="msg">Confirmation du mot de passe :</label>
-            <input type="password" id="pass2" name="password2"
-            minlength="8" required><?php echo $confpasswordErr;?>
-          </div>
+            <input type="password" id="pass2" name="password2" required
+            ><?php echo $confpasswordErr;?>
                   
-                  <button type="submit" name="inscription">Sign in</button>
+                  <button class="boutoninscription" type="submit" name="inscription">Sign in</button>
                   <div>Vous avez déjà un compte ? </div>
-                  <button type="submit" name="connexion">Log in</button>
+                  <button class="boutoninscription" type="submit" name="connexion">Log in</button>
           </form>
+        </div></div>
+        </section>
   </article>
   <article class="aside_inscription">
   <!-- <img id="img" src="Images/leafpattern.jpeg" class="aside_inscription"> -->
   </article>
   <article id="icon_inscription">
-    <div><img src="Images/cercle1.png" class="cercle"></div>
-    <div><img src="Images/cercle2.png" class="cercle"></div>
-    <div><img src="Images/cercle3.png" class="cercle"></div>
+    <div id="cercle1"></div>
+    <div id="cercle2"></div>
+    <div id="cercle3"></div>
   </article>
             </main>
             
